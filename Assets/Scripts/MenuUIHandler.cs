d1133ede71457e20713b2f89a9b2e9e23561745a
Jceek <guter.seek@gmail.com>
2025-08-25 22:08:26 +0200

    fixed the project with someone elses code

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    //This is the handler of the main menu scene
+    [SerializeField] TextMeshProUGUI PlayerNameInput;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SetPlayerName()
+    {
+        PlayerDataHandle.Instance.PlayerName = PlayerNameInput.text;
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit()
+#endif
+    }
+}
